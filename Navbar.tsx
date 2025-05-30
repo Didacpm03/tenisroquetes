7a4fe0799cf2b9d0517f272b1546eaaef89a780f
Didacpm03 <aresyt123@gmail.com>
2025-05-30 14:26:24 +0200

    canvi titol

--- Navbar.tsx
+++ Navbar.tsx
@@ -47,7 +47,7 @@ const Navbar: React.FC<NavbarProps> = ({ onCartClick }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="text-2xl font-bold tracking-tighter">
-            SNKR<span className="text-orange-500">HUB</span>
+            O<span className="text-orange-500">HUB</span>
           </Link>
 
           {/* Desktop navigation */}
